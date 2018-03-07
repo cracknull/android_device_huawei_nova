a6d73466666f53170ba172d97faa8d0e9b28d477
crackevil <crackevil@gmail.com>
2018-03-07 15:43:17 +0800

    repo basic update

---
+++ rootfs.bat
@@ -0,0 +1,10 @@
+@echo off
+mkdir ramdisk\data >nul 2>&1
+mkdir ramdisk\etc >nul 2>&1
+mkdir ramdisk\dev >nul 2>&1
+mkdir ramdisk\proc >nul 2>&1
+mkdir ramdisk\oem >nul 2>&1
+mkdir ramdisk\tmp >nul 2>&1
+mkdir ramdisk\system >nul 2>&1
+mkdir ramdisk\vendor >nul 2>&1
+mkdir ramdisk\sbin >nul 2>&1
